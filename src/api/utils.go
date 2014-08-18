efd5d3280f50e6e2538bd52b2447b355fd30e781
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-08-18 12:18:56 +0300

    with the previous commit we don't need those anymore

--- src/api/utils.go
+++ src/api/utils.go
@@ -9,22 +9,6 @@ func RenderJson(resp http.ResponseWriter, json interface{}) {
 	Render.JSON(resp, http.StatusOK, json)
 }
 
-type ErrorJson struct {
-	Code int
-	Name string
-}
-
-func authFailed() ErrorJson {
-	return ErrorJson{http.StatusUnauthorized, "Authorization Failed"}
-}
-
-type myFunc func() ErrorJson
-
-func RenderError(jsonError myFunc, resp http.ResponseWriter) {
-	err := jsonError()
-	Render.JSON(resp, err.Code, map[string]string{"error": err.Name})
-}
-
 func checkQuery(err error) (bool) {
 	if err == sql.ErrNoRows || err != nil {
 		return false
