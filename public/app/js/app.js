ac3e4b3c7e4d29a9041f9b89e9f323bebc3af41c
Antonis Tsiapaliokas <antonis.tsiapaliokas@kde.org>
2014-10-11 15:13:26 +0300

    Port ngRouter to ui-router

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -2,18 +2,21 @@
 
 // Declare app level module which depends on views, and components
 var modipApp = angular.module('modipApp', [
-  'ngRoute',
+  'ui.router',
   'modipControllers',
   'modipServices'
 ]);
 
-modipApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/', {
+modipApp.config(['$stateProvider', '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    // For any unmatched url, redirect to /
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider.
+      state('home', {
+        url: '/',
         templateUrl: 'app/templates/home/home.html',
         controller: 'HomeCtrl'
-      }).
-      otherwise({redirectTo: '/'})
+      })
   }
 ]);
