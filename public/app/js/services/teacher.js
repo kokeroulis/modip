98f4b1dbfe38ec23c8404f2f59eebb1395dbdd2b
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-10-11 13:25:34 +0300

    add moched api

--- public/app/js/services/teacher.js
+++ public/app/js/services/teacher.js
@@ -21,8 +21,83 @@ modipServices.factory('TeacherService', ['$http', '$q', function($http, $q) {
     return deffered.promise;
   }
 
+  function info() {
+    /*var deffered = $q.defer();
+    $http.get('teacher/info').success(function (result) {
+      deffered.resolve(result.data);
+    }).error(function(data, status) {
+      deffered.reject({
+        body: data.Common.error,
+        status: status
+      });
+    });
+    return deffered.promise;*/
+    return {
+      teacher: {
+        id:1,
+        name:'superteacher1',
+        email:'superteacher1@teilar.gr',
+        department: {
+          id:1,
+          name: 'T.P.T.'
+        }
+      },
+      assetTypes: [
+        {
+          id :1
+          name: 'books',
+          count: 10,
+          assets: [
+            {
+              id: 1,
+              assetType: 1
+              content: 'Super Book'
+            }
+          ]
+        }
+      ]
+    }
+  }
+
+  function deleteAsset(assetId) {
+    return {
+      id: 1,
+      content: 'Super Book',
+      assetType: 1
+    }
+  }
+
+  function moveAsset(assetId, newAssetTypeId) {
+    return {
+      id: 1,
+      content: 'Super Book',
+      assetType: 2
+    }
+  }
+
+  function modifyAsset(assetId, newContent) {
+    return {
+      id: 1,
+      content: 'Modified Awesome Book',
+      assetType: 1
+    }
+  }
+
+  function addAsset(content, assetTypeId) {
+    return {
+      id: 2,
+      content: 'Uber Book',
+      assetType: 1
+    }
+  }
+
   var service = {
-    login: login
+    login: login,
+    info: info,
+    deleteAsset: deleteAsset,
+    moveAsset: moveAsset,
+    modifyAsset: modifyAsset,
+    addAsset: addAsset
   };
 
   return service;
