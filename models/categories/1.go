b3e5f01be10c7b9ec400e1be2db2ab232e5a766d
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-10-25 13:27:07 +0300

    add real data

--- models/categories/1.go
+++ models/categories/1.go
@@ -4,11 +4,14 @@ import "github.com/kokeroulis/modip/models"
 
 func init1() {
 	g := models.CategoryGroup {
-		Name: "Group1",
+		Name: "Διδάσκων",
 	}
 
-	parent := newParentCategoryForTeacher(1, "foo", g)
-	parent.AddChild(newCategory(1001, "fooChild"))
-	parent.AddChild(newCategory(1002, "fooChild2"))
+	parent := newParentCategoryForTeacher(1, "Α.Δ. Εκπ. Προσωπικού", g)
+	parent.AddChild(newCategory(1001, "Αριθμός Δημοσιεύσεων"))
+	parent.AddChild(newCategory(1002, "Αναγνώριση του επιστημονικού και άλλου έργου"))
+	parent.AddChild(newCategory(1003, "Ερευνητικά προγράμματα και έργα"))
+	parent.AddChild(newCategory(1004, "Ερευνητικές Υποδομές"))
+	parent.AddChild(newCategory(1005, "Σύνδεση με την κοινωνία"))
 }
 
