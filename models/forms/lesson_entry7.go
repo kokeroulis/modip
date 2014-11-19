936877ce7f02669aa0379b0474a23f0f3b9e7fbb
Alex-P. Natsios <drakevr@2f30.org>
2014-11-19 12:40:37 +0200

    Add form entry #7

---
+++ models/forms/lesson_entry7.go
@@ -0,0 +1,12 @@
+package forms
+
+import (
+	 "github.com/kokeroulis/modip/db"
+)
+
+type LessonCreateReportFormEntry7 struct {
+	// Επικοινωνία & Καθοδήγηση Σπουδαστών / Συνεργασίες
+	Field1 string `schema:"lesson_epikoinonia_wres_grafeiou"`
+	Field2 string `schema:"lesson_epikoinonia_ekepedeusi_methodos"`
+	Field3 string `schema:"lesson_epikoinonia_organosi_ekpedeutikon_episkepseon"`
+}
