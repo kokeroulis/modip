491cf094c602cf94c44c7649684503f91d2e4c80
Alex-P. Natsios <drakevr@2f30.org>
2014-11-19 13:08:11 +0200

    Add form entry #10

---
+++ models/forms/lesson_entry10.go
@@ -0,0 +1,17 @@
+package forms
+
+import (
+	_"github.com/kokeroulis/modip/db"
+)
+
+type LessonCreateReportFormEntry10 struct {
+	// Διαθέσιμη εκπαιδευτική υποδομή του μαθήματος
+	Field1 string `schema:"dia8esimi_ekpedeutikh_ypodomh_tou_ma8imatos_ai8ouses_didaskalias_pou_xrisimopoiountai"`
+	Field2 string `schema:"dia8esimi_ekpedeutikh_ypodomh_tou_ma8imatos_ergasthria_pou_xrhsimopoiountai_gia_to_sugkekrimeno_ma8ima"`
+	Field3 string `schema:"dia8esimi_ekpedeutikh_ypodomh_tou_ma8imatos_einai_dia8esima_ta_ergasthria_tou_ma8imatos_gia_xrisi_ektos_programmatismenon_wrwn"`
+	Field4 string `schema:"dia8esimi_ekpedeutikh_ypodomh_tou_ma8imatos_spoudasthria"`
+	Field5 string `schema:"dia8esimi_ekpedeutikh_ypodomh_tou_ma8imatos_xrhsimopoihtai_ekpedeutiko_logismiko_kai_poio"`
+	Field6 string `schema:"dia8esimi_ekpedeutikh_ypodomh_tou_ma8imatos_yparxei_ikanopoihtiki_ypostiriksi_tou_ma8imatos_apo_tin_biblio8iki"`
+	Field7 string `schema:"dia8esimi_ekpedeutikh_ypodomh_tou_ma8imatos_pws_krinete_sunolika_tin_dia8esimi_ekpedeutiki_ypodomi"`
+	Field8 string `schema:"dia8esimi_ekpedeutikh_ypodomh_tou_ma8imatos_"`
+}
