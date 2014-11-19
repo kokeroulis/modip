854add701d6a28aba62b6668cf30e4951e0c7d45
Alex-P. Natsios <drakevr@2f30.org>
2014-11-19 12:58:42 +0200

    Add form entry #9

---
+++ models/forms/lesson_entry9.go
@@ -0,0 +1,20 @@
+package forms
+
+import (
+	 _"github.com/kokeroulis/modip/db"
+)
+
+type LessonCreateReportFormEntry5 struct {
+	// 
+	Field1 bool `schema:"lesson_aksiologisi_ths_epidosis_grapti_eksetasi"`
+	Field2 bool `schema:"lesson_aksiologisi_ths_epidosis_proforiki_eksetasi"`
+	Field3 bool `schema:"lesson_aksiologisi_ths_epidosis_proodos"`
+	Field4 bool `schema:"lesson_aksiologisi_ths_epidosis_katoi_kon_ergasia"`
+	Field5 bool `schema:"lesson_aksiologisi_ths_epidosis_proforiki_parousiasi_ergasias"`
+	Field6 bool `schema:"lesson_aksiologisi_ths_epidosis_ergashtirio_praktikes_askiseis"`
+	Field7 bool `schema:"lesson_aksiologisi_ths_epidosis_alla"`
+	Field8 string `schema:"lesson_aksiologisi_ths_epidosis_epeksigiseis_alla"`
+	Field9 bool `schema:"lesson_aksiologisi_ths_epidosis_parakolou8ish_spoudaston_sto_erg"`
+	Field10 bool `schema:"lesson_aksiologisi_ths_epidosis_sxolia_mesa_eksaminou"`
+	Field11 string `schema:"lesson_aksiologisi_ths_epidosis_diafania_aksiologisis_tis_epidosis"`
+}
