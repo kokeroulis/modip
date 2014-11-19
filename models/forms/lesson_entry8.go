90f04c89d3457155c93b1ddf48c7fe891f35305d
Alex-P. Natsios <drakevr@2f30.org>
2014-11-19 12:47:42 +0200

    Add form entry #8

---
+++ models/forms/lesson_entry8.go
@@ -0,0 +1,11 @@
+package forms
+
+import (
+	 "github.com/kokeroulis/modip/db"
+)
+
+type LessonCreateReportFormEntry8 struct {
+	// Συμμετοχή Σπουδαστών
+	Field1 string `schema:"lesson_summetoxi_spoudaston_summetoxi_stin_kanoniki_eksetasi"`
+	Field2 int `schema:"lesson_summetoxi_spoudaston_summetoxi_stin_epanaliptikh_eksetasi"`
+}
