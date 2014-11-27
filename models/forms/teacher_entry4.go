ced451fc11ce5ab78d81d806ed341d6ee1a3356f
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-11-27 13:42:08 +0200

    try to fix the queries

--- models/forms/teacher_entry4.go
+++ models/forms/teacher_entry4.go
@@ -25,18 +25,34 @@ type TeacherCreateReportFormEntry4 struct {
 
 func (f *TeacherCreateReportFormEntry4) Update() {
 	query := `UPDATE TeacherCreateReportFormEntry4
-			  SET author = $1, title = $2, is_magazine $3,
-			  publisher = $4,
-			  publication_date = $5, type = $5
-			  WHERE id = $2`
+			  SET
+				ereunitikes_ypodomes_arithmos_kai_xwritikotita_ereunitikon_ergasthrion_pou_xrisimopoieitai = $1,
+				ereunitikes_ypodomes_eparkeia_katalilotita_kai_poiotita_ton_ereunitikon_ergasthrion = $2,
+				ereunitikes_ypodomes_eparkeia_katallilotita_kai_poithta_tou_ergasthriakou_eksoplismou = $3,
+				ereunitikes_ypodomes_kalyptoun_oi_dia8esimes_ypodomes_tis_anagkes_tis_ereunhtikis_diadikasias = $4,
+				ereunitikes_ypodomes_poia_apo_ta_ereunitika_sas_antikeimena_den_kalyptontai_apo_tis_ypodomes = $5,
+				ereunitikes_ypodomes_poso_entatiki_xrish_kanete_ton_sugkrekrimenon_ereunitikon_ypodomon = $6,
+				ereunitikes_ypodomes_ananeosi_ereunitikon_ypodomon = $7,
+				ereunitikes_ypodomes_pws_epidiokete_th_xrimatodothsh_gia_promi8eia = $8,
+				ereunitikes_ypodomes_praktiki_akiopoihsh_ton_ereunitikon_apotelesmaton = $9,
+			 WHERE id = $10`
 
-	err := Db.Database.Exec(query, f.Id)
+	err := Db.Database.Exec(query,
+							f.Field1,
+							f.Field2,
+							f.Field3,
+							f.Field4,
+							f.Field5,
+							f.Field6,
+							f.Field7,
+							f.Field8,
+							f.Field9,
+							f.Id)
 
 	Db.CheckQueryWithNoRows(err, query)
 
 	if len(f.Helpers) > 0 {
 		// we have helpers lets add them
-
 		for _, it := range f.Helpers {
 			query = `INSERT INTO TeacherCreateReportFormEntry4Helper
 					 (form, content) VALUES ($1, $2)`
@@ -48,16 +64,40 @@ func (f *TeacherCreateReportFormEntry4) Update() {
 	}
 }
 
-func ListAllTeacherCreateReportForm1(teacherId int) []TeacherCreateReportFormEntry4 {
-	var formList []TeacherCreateReportFormEntry4
-
-	query := `SELECT TeacherCreateReportFormEntry4
-			  id, author, title, is_magazine,
-			  publisher, publication_date
+func (f *TeacherCreateReportFormEntry4) Load(teacherId int) {
+	query := `SELECT
+				ereunitikes_ypodomes_arithmos_kai_xwritikotita_ereunitikon_ergasthrion_pou_xrisimopoieitai,
+				ereunitikes_ypodomes_eparkeia_katalilotita_kai_poiotita_ton_ereunitikon_ergasthrion,
+				ereunitikes_ypodomes_eparkeia_katallilotita_kai_poithta_tou_ergasthriakou_eksoplismou,
+				ereunitikes_ypodomes_kalyptoun_oi_dia8esimes_ypodomes_tis_anagkes_tis_ereunhtikis_diadikasias,
+				ereunitikes_ypodomes_poia_apo_ta_ereunitika_sas_antikeimena_den_kalyptontai_apo_tis_ypodomes,
+				ereunitikes_ypodomes_poso_entatiki_xrish_kanete_ton_sugkrekrimenon_ereunitikon_ypodomon,
+				ereunitikes_ypodomes_ananeosi_ereunitikon_ypodomon,
+				ereunitikes_ypodomes_pws_epidiokete_th_xrimatodothsh_gia_promi8eia,
+				ereunitikes_ypodomes_praktiki_akiopoihsh_ton_ereunitikon_apotelesmaton,
 			  FROM TeacherCreateReportFormEntry4
-			  WHERE teacher $1`
+			  WHERE id = $1 AND teacher = $2`
 
-	rows, err := Db.Database.Query(query, teacherId)
+	err := Db.Database.QueryRow(query, f.Id, teacherId).
+			Scan(&f.Field1,
+				&f.Field2,
+				&f.Field3,
+				&f.Field4,
+				&f.Field5,
+				&f.Field6,
+				&f.Field7,
+				&f.Field8,
+				&f.Field9)
+
+	Db.CheckQuery(err, query)
+
+	var helpers []TeacherCreateReportFormEntry4Helper
+
+	query := `SELECT content
+			  FROM TeacherCreateReportFormEntry4Helper
+			  WHERE form = $1`
+
+	rows, err = Db.Database.Query(query, f.Id)
 
 	if err != nil {
 		panic(err)
@@ -65,20 +105,13 @@ func ListAllTeacherCreateReportForm1(teacherId int) []TeacherCreateReportFormEnt
 
 	defer rows.Close()
 
-
 	for rows.Next() {
-		it := TeacherCreateReportFormEntry4{}
+		it := TeacherCreateReportFormEntry4Helper{}
 
-		if err := rows.Scan(&it.Id,
-							&it.Field1,
-							&it.Field2,
-							&it.Field3,
-							&it.Field4,
-							&it.Field5,
-							&it.Field6); err != nil {
+		if err = rows.Scan(&it.Content); err != nil {
 			panic(err)
 		} else {
-			formList = append(formList, it)
+			helpers = append(helpers, it)
 		}
 	}
 
@@ -86,5 +119,5 @@ func ListAllTeacherCreateReportForm1(teacherId int) []TeacherCreateReportFormEnt
 		panic(err)
 	}
 
-	return formList
+	f.Helpers = helpers
 }
