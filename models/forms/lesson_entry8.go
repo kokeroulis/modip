8808baae69bcdc0f284cdd10d2f93bb19a5c6c9c
Alex-P. Natsios <drakevr@2f30.org>
2014-11-27 09:21:40 +0200

    fix lesson_entry8.go

--- models/forms/lesson_entry8.go
+++ models/forms/lesson_entry8.go
@@ -1,7 +1,7 @@
 package forms
 
 import (
-	_"github.com/kokeroulis/modip/db"
+	"github.com/kokeroulis/modip/db"
 )
 
 type LessonCreateReportFormEntry8 struct {
@@ -10,41 +10,29 @@ type LessonCreateReportFormEntry8 struct {
 	Field2 int `schema:"lesson_summetoxi_spoudaston_summetoxi_stin_epanaliptikh_eksetasi"`
 }
 
-=============== Start create table
-CREATE TABLE LessonCreateReportFormEntry8 (
-	lesson int references lesson(id) on delete cascade,
-	lesson_summetoxi_spoudaston_summetoxi_stin_kanoniki_eksetasi text,
-	lesson_summetoxi_spoudaston_summetoxi_stin_epanaliptikh_eksetasi int
-)
-=============== End Create table
-=============== Start create update func
 func (f *LessonCreateReportFormEntry8) Update(lessonId int) {
-query := `UPDATE LessonCreateReportFormEntry8 SET 
+query := `UPDATE LessonCreateReportFormEntry8 SET
 	lesson_summetoxi_spoudaston_summetoxi_stin_kanoniki_eksetasi = $1,
 	lesson_summetoxi_spoudaston_summetoxi_stin_epanaliptikh_eksetasi = $2
 WHERE lesson = $`
 
-_, err := Db.Database.Exec(query, 
-	Field1, 
-	Field2
-)
+_, err := Db.Database.Exec(query,
+	f.Field1,
+	f.Field2)
 
 Db.CheckQueryWithNoRows(err, query)
 }
-=============== End Create update func
-=============== Start create select func
+
 func (f *LessonCreateReportFormEntry8) Load(lessonId int) {
-query := `SELECT 
+query := `SELECT
 	lesson_summetoxi_spoudaston_summetoxi_stin_kanoniki_eksetasi,
 	lesson_summetoxi_spoudaston_summetoxi_stin_epanaliptikh_eksetasi
 FROM LessonCreateReportFormEntry8
 WHERE lesson = $`
 
-err := Db.Database.QueryRow(query, TODO). 
+err := Db.Database.QueryRow(query, lessonId).
 	Scan(&f.Field1,
-		&f.Field2
-)
+		&f.Field2)
 
 Db.CheckQueryWithNoRows(err, query)
 }
-=============== End Create select func
