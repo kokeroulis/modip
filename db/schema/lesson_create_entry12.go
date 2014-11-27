efef5ec7f1c7efda429994712b975de3461ca125
Dimitris Papapoulios <skiarxon@gmail.com>
2014-11-27 10:07:00 +0200

    add lesson_create_entry12.go

---
+++ db/schema/lesson_create_entry12.go
@@ -0,0 +1,10 @@
+package DbSchema
+
+const LessonCreateReportFormEntry12 = `
+CREATE TABLE lessoncreatereportformentry12 (
+	lesson                      int references lesson(id) on delete cascade,
+	ekpedeutika_mesa_xrhsh_ekpedeutikon_meson              bool default '0',
+	ekpedeutika_mesa_eparkeia_ekpedeutikon_meson           bool default '0',
+	ekpedeutika_mesa_anafora_elleipseon_ekpedeutikon_meson text default ' '
+)
+`
