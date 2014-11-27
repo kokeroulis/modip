d32f71aef61fa58d9fe65a7ce987999860e2adcf
Dimitris Papapoulios <skiarxon@gmail.com>
2014-11-27 10:15:50 +0200

    add lesson_create_entry14.go

---
+++ db/schema/lesson_create_entry14.go
@@ -0,0 +1,14 @@
+package DbSchema
+
+const LessonCreateReportFormEntry14 = `
+CREATE TABLE LessonCreateReportFormEntry14 (
+	lesson                                  int references lesson(id) on delete cascade,
+	katanomi_ba8mon_spoudaston_0_39                                     int default '0',
+	katanomi_ba8mon_spoudaston_4_49                                     int default '0',
+	katanomi_ba8mon_spoudaston_5_59                                     int default '0',
+	katanomi_ba8mon_spoudaston_6_69                                     int default '0',
+	katanomi_ba8mon_spoudaston_7_84                                     int default '0',
+	katanomi_ba8mon_spoudaston_85_10                                    int default '0',
+	katanomi_ba8mon_spoudaston_mesos_oros_ba8mologias_sunolo_spoudaston int default '0'
+)
+`
