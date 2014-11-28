65850c3b1941bef4105e3f4381f7d337dd4b97ea
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-11-28 23:18:29 +0200

    lesson entry2 table ++

--- db/schema/lesson_create_entry2.go
+++ db/schema/lesson_create_entry2.go
@@ -7,6 +7,20 @@ CREATE TABLE LessonCreateReportFormEntry2 (
 	lesson_didaskalia_typos_ma8iomatos2 text default ' ',
 	lesson_didaskalia_typos_ma8iomatos3 text default ' ',
 	lesson_didaskalia_typos_ma8iomatos4 text default ' ',
-	lesson_didaskalia_typos_ma8iomatos5 text default ' '
+	lesson_didaskalia_typos_ma8iomatos5 text default ' ',
+	lesson_didaskalia_wres_didaskaliasi_8  text default ' ',
+	lesson_didaskalia_wres_didaskalias_AP text default ' ',
+	lesson_didaskalia_wres_didaskalias_E text default ' ',
+	lesson_didaskalia_monadesECTS_8AP text default ' ',
+	lesson_didaskalia_monadesECTS_E text default ' ',
+	lesson_didaskalia_wres_didaskalias_ana_bdomada text default ' ',
+	lesson_didaskalia_wres_ergasthriou_askhshs boolean default false,
+	lesson_didaskalia_wres_ergasthriou_ana_bdomada text default ' ',
+	lesson_didaskalia_problepomenes_wres_ergasthriou_ana_eksamino text default ' ',
+	lesson_didaskalia_problepomenes_wres_didaskalias_mikron_omadon_ana_eksamino text default ' ',
+	lesson_didaskalia_problepomenes_wres_didaskalias_ana_eksamino_alli text default ' ',
+	lesson_didaskalia_pollapli_bibiografia boolean default false,
+	lesson_didaskalia_diati8ete_ergasia_proodos boolean default false,
+	lesson_didaskalia_ypoxreotiki_ergasia_proodos boolean default false
 )
 `
