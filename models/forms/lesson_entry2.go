9d702c99ea629c24b9e1cf82d4d1898cafa712f0
Antonis Tsiapaliokas <antonis.tsiapaliokas@kde.org>
2014-11-28 23:25:31 +0200

    Fix query ++

--- models/forms/lesson_entry2.go
+++ models/forms/lesson_entry2.go
@@ -77,25 +77,25 @@ func (f *LessonCreateReportFormEntry2) Update(lessonId int) {
 
 func (f *LessonCreateReportFormEntry2) Load(lessonId int) {
 query := `SELECT
-             lesson_didaskalia_typos_ma8iomatos1 = $ 1,
-             lesson_didaskalia_typos_ma8iomatos2 = $2,
-             lesson_didaskalia_typos_ma8iomatos3 = $3,
-             lesson_didaskalia_typos_ma8iomatos4 = $4,
-             lesson_didaskalia_typos_ma8iomatos5 = $5,
-             lesson_didaskalia_wres_didaskaliasi_8 = $6,
-             lesson_didaskalia_wres_didaskalias_AP = $7,
-             lesson_didaskalia_wres_didaskalias_E = $8,
-             lesson_didaskalia_monadesECTS_8AP = $9,
-             lesson_didaskalia_monadesECTS_E = $10,
-             lesson_didaskalia_wres_didaskalias_ana_bdomada = $11,
-             lesson_didaskalia_wres_ergasthriou_askhshs = $12,
-             lesson_didaskalia_wres_ergasthriou_ana_bdomada = $13,
-             lesson_didaskalia_problepomenes_wres_ergasthriou_ana_eksamino = $14,
-             lesson_didaskalia_problepomenes_wres_didaskalias_mikron_omadon_ana_eksamino = $15,
-             lesson_didaskalia_problepomenes_wres_didaskalias_ana_eksamino_alli = $16,
-             lesson_didaskalia_pollapli_bibiografia = $17,
-             lesson_didaskalia_diati8ete_ergasia_proodos = $18,
-             lesson_didaskalia_ypoxreotiki_ergasia_proodos = $19
+             lesson_didaskalia_typos_ma8iomatos1,
+             lesson_didaskalia_typos_ma8iomatos2,
+             lesson_didaskalia_typos_ma8iomatos3,
+             lesson_didaskalia_typos_ma8iomatos4,
+             lesson_didaskalia_typos_ma8iomatos5,
+             lesson_didaskalia_wres_didaskaliasi_8,
+             lesson_didaskalia_wres_didaskalias_AP,
+             lesson_didaskalia_wres_didaskalias_E,
+             lesson_didaskalia_monadesECTS_8AP,
+             lesson_didaskalia_monadesECTS_E,
+             lesson_didaskalia_wres_didaskalias_ana_bdomada,
+             lesson_didaskalia_wres_ergasthriou_askhshs,
+             lesson_didaskalia_wres_ergasthriou_ana_bdomada,
+             lesson_didaskalia_problepomenes_wres_ergasthriou_ana_eksamino,
+             lesson_didaskalia_problepomenes_wres_didaskalias_mikron_omadon_ana_eksamino,
+             lesson_didaskalia_problepomenes_wres_didaskalias_ana_eksamino_alli,
+             lesson_didaskalia_pollapli_bibiografia,
+             lesson_didaskalia_diati8ete_ergasia_proodos,
+             lesson_didaskalia_ypoxreotiki_ergasia_proodos
 		 FROM LessonCreateReportFormEntry2
 		 WHERE lesson = $1`
 
