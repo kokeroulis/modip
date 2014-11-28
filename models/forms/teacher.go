3deba6887c9204959e4e3778d7ab92daa6a81599
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-11-28 16:07:19 +0200

    load all the teacher forms

--- models/forms/teacher.go
+++ models/forms/teacher.go
@@ -2,7 +2,7 @@ package forms
 
 type TeacherCreateReportForm struct {
 	TeacherId int
-	Entry1    TeacherCreateReportFormEntry1
+	Entry1    []TeacherCreateReportFormEntry1
 	Entry2    TeacherCreateReportFormEntry2
 	Entry3    TeacherCreateReportFormEntry3
 	Entry4    TeacherCreateReportFormEntry3
@@ -10,10 +10,9 @@ type TeacherCreateReportForm struct {
 }
 
 func (f *TeacherCreateReportForm) Load() {
-/*	f.Entry1.Load(f.TeacherId)
+	f.Entry1 = ListAllTeacherCreateReportForm1(f.TeacherId)
 	f.Entry2.Load(f.TeacherId)
 	f.Entry3.Load(f.TeacherId)
 	f.Entry4.Load(f.TeacherId)
 	f.Entry5.Load(f.TeacherId)
-	*/
 }
