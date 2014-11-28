4a79b1dd0c315e691e3aab94fb38a8712d0e85f5
Antonis Tsiapaliokas <antonis.tsiapaliokas@kde.org>
2014-11-28 21:43:35 +0200

    Fix the routes. Routes with was getting as an id parameter didn't work when the id > 9. Because the reg exp was wrong. Documenting it here for future purposes.
    [0-9] == from 0 to 9
    [1-9] == from 1 to 9
    [0-9]+ == from 01 to 09
    [1-9]+ == from 1 to 90  -----> 90% we want to use this one

--- api/routes.go
+++ api/routes.go
@@ -37,7 +37,7 @@ func setupRoutes(n *negroni.Negroni) {
 	router.Handle("/teacher/report", authorize{GetTeacherCreateReport}).Methods("GET")
 	router.Handle("/teacher/report/1", authorize{TeacherCreateReport1}).Methods("POST")
 	router.Handle("/teacher/report/1/edit", authorize{GetTeacherCreateReport1Edit}).Methods("GET")
-	router.Handle("/teacher/report/1/edit/{id:[0-9]}", authorize{TeacherCreateReport1Edit}).Methods("POST")
+	router.Handle("/teacher/report/1/edit/{id:[1-9]+}", authorize{TeacherCreateReport1Edit}).Methods("POST")
 
 
 	router.Handle("/teacher/report/{id:[2-5]}", authorize{TeacherCreateReport}).Methods("POST")
@@ -47,14 +47,14 @@ func setupRoutes(n *negroni.Negroni) {
 	router.Handle("/research/program/create/report", authorize{GetResearchProgramCreateReport}).Methods("GET")
 	router.Handle("/research/program/create/report", authorize{ResearchProgramCreateReport}).Methods("POST")
 
-	router.Handle("/research/program/edit/report/{id:[0-9]}", authorize{GetResearchProgramEditReport}).Methods("GET")
-	router.Handle("/research/program/edit/report/{id:[0-9]}", authorize{ResearchProgramEditReport}).Methods("POST")
+	router.Handle("/research/program/edit/report/{id:[1-9]+}", authorize{GetResearchProgramEditReport}).Methods("GET")
+	router.Handle("/research/program/edit/report/{id:[1-9]+}", authorize{ResearchProgramEditReport}).Methods("POST")
 
 	// Α.Δ. Μαθημάτων Π.Π.Σ.
 	router.Handle("/lesson/list/pre/degree", authorize{LessonListPreDegree}).Methods("GET")
 	router.Handle("/lesson/list/pre/degree/{id:[1-9]+}", authorize{LessonListPreDegreeDepartment}).Methods("GET")
-	router.Handle("/lesson/pre/degree/create/report/{lesson_id:[0-9]+}", authorize{GetLessonPreDegreeCreateReport}).Methods("GET")
-	router.Handle("/lesson/pre/degree/create/report/{id:[0-9]+}/{lesson_id:[0-9]+}", authorize{LessonPreDegreeCreateReport}).Methods("POST")
+	router.Handle("/lesson/pre/degree/create/report/{lesson_id:[1-9]+}", authorize{GetLessonPreDegreeCreateReport}).Methods("GET")
+	router.Handle("/lesson/pre/degree/create/report/{id:[1-9]+}/{lesson_id:[1-9]+}", authorize{LessonPreDegreeCreateReport}).Methods("POST")
 
 	router.Handle("/lesson/list/post/degree", authorize{LessonListPostDegree}).Methods("GET")
 
