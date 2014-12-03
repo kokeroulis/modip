d9e7eeebf4eb059fc51a098814ae0f450ceb9303
Alex-P. Natsios <drakevr@2f30.org>
2014-12-03 13:16:53 +0200

    Fix form route regexp

--- api/routes.go
+++ api/routes.go
@@ -61,7 +61,6 @@ func setupRoutes(n *negroni.Negroni) {
 	router.Handle("/lesson/list/pre/degree/{id:[1-9]+}/{akademicYearId:[1-9]+}", authorize{LessonListPreDegreeDepartment}).Methods("GET")
 	router.Handle("/lesson/pre/degree/create/report/{lesson_id:[0-9]+}/{akademicYearId:[1-9]+}", authorize{GetLessonPreDegreeCreateReport}).Methods("GET")
 	router.Handle("/lesson/pre/degree/create/report/{id:[0-9]+}/{lesson_id:[0-9]+}/{akademicYearId:[1-9]+}", authorize{LessonPreDegreeCreateReport}).Methods("POST")
-
 	router.Handle("/lesson/list/post/degree", authorize{LessonListPostDegree}).Methods("GET")
 
 
