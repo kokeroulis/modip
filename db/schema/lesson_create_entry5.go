c110ccb893d72d72feb190d964d91f6f4d4f8be9
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-12-04 18:05:38 +0200

    add the akademic_year into the tables

--- db/schema/lesson_create_entry5.go
+++ db/schema/lesson_create_entry5.go
@@ -3,6 +3,7 @@ package DbSchema
 const LessonCreateReportFormEntry5 = `
 CREATE TABLE LessonCreateReportFormEntry5 (
 	lesson int references lesson(id) on delete cascade,
+	akademic_year int references akademic_year(id) on delete cascade,
 	lesson_didaktika_boi8imata_boi8imata_pou_dianemontai_stous_foithtes text default ' ',
 	lesson_didaktika_boi8imata_epikairopoihsh_ton_boh8imaton text default ' ',
 	lesson_didaktika_boi8imata_pososto_didaskomenis_ylis_pou_kaliptete text default ' ',
