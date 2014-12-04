d1da80abe3412597c30aaa33dd5ee757ee438e43
Giorgos Tsiapaliokas <giorgos.tsiapaliokas@kde.org>
2014-12-04 18:45:37 +0200

    add akademic_year for teacher and lesson

--- db/api/akademic_year.go
+++ db/api/akademic_year.go
@@ -4,6 +4,9 @@ const AkademicYear = `
 CREATE OR REPLACE FUNCTION akademic_year_create(akademic_year_name text,
 										 out alreadyExists boolean) AS $$
 DECLARE
+	akademic_year_id int;
+	lessonId int;
+	teacherId int;
 BEGIN
 	PERFORM * FROM akademic_year
 	WHERE name = akademic_year_name;
@@ -17,7 +20,58 @@ BEGIN
 
 	INSERT INTO akademic_year
 	(name)
-	VALUES (akademic_year_name);
+	VALUES (akademic_year_name)
+	RETURNING id INTO akademic_year_id;
+
+	FOR lessonId IN SELECT id FROM lesson LOOP
+		INSERT INTO LessonCreateReportFormEntry1
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
+
+		INSERT INTO LessonCreateReportFormEntry2
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
+
+		INSERT INTO LessonCreateReportFormEntry3
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
+
+		INSERT INTO LessonCreateReportFormEntry4
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
+
+		INSERT INTO LessonCreateReportFormEntry5
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
+
+		INSERT INTO LessonCreateReportFormEntry11
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
+
+		INSERT INTO LessonCreateReportFormEntry12
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
+
+		INSERT INTO LessonCreateReportFormEntry13
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
+
+		INSERT INTO LessonCreateReportFormEntry14
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
+
+
+		INSERT INTO LessonCreateReportFormEntry15
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
+
+		INSERT INTO LessonCreateReportFormEntry16
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
+	END LOOP;
+
+	FOR teacherId IN SELECT id FROM teacher LOOP
+		INSERT INTO TeacherCreateReportFormEntry2
+		(teacher, akademic_year) VALUES (teacherId, akademic_year_id);
+
+		INSERT INTO TeacherCreateReportFormEntry3
+		(teacher, akademic_year) VALUES (teacherId, akademic_year_id);
+
+		INSERT INTO TeacherCreateReportFormEntry4
+		(teacher, akademic_year) VALUES (teacherId, akademic_year_id);
+
+		INSERT INTO TeacherCreateReportFormEntry5
+		(teacher, akademic_year) VALUES (teacherId, akademic_year_id);
+	END LOOP;
 END;
 $$ LANGUAGE plpgsql;
 `
--- db/api/lesson.go
+++ db/api/lesson.go
@@ -6,6 +6,7 @@ CREATE OR REPLACE FUNCTION lesson_create(lessonName text, departmentId int,
 										 cdCode text, OUT alreadyExists boolean) AS $$
 DECLARE
 	lessonId int;
+	akademic_year_id int;
 BEGIN
 	PERFORM * FROM lesson
 	WHERE name = lessonName AND department = departmentId;
@@ -22,51 +23,56 @@ BEGIN
 	VALUES (lessonName, departmentId, is_post_degree, courseCodeLesson, cdCode)
 	RETURNING id INTO lessonId;
 
-	INSERT INTO LessonCreateReportFormEntry1
-	(lesson) VALUES (lessonId);
+	FOR akademic_year_id IN SELECT id FROM akademic_year LOOP
+		INSERT INTO LessonCreateReportFormEntry1
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
 
-	INSERT INTO LessonCreateReportFormEntry2
-	(lesson) VALUES (lessonId);
+		INSERT INTO LessonCreateReportFormEntry2
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
 
-	INSERT INTO LessonCreateReportFormEntry3
-	(lesson) VALUES (lessonId);
+		INSERT INTO LessonCreateReportFormEntry3
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
 
-	INSERT INTO LessonCreateReportFormEntry4
-	(lesson) VALUES (lessonId);
+		INSERT INTO LessonCreateReportFormEntry4
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
 
-	INSERT INTO LessonCreateReportFormEntry5
-	(lesson) VALUES (lessonId);
+		INSERT INTO LessonCreateReportFormEntry5
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
 
-	INSERT INTO LessonCreateReportFormEntry6
-	(lesson) VALUES (lessonId);
+                INSERT INTO LessonCreateReportFormEntry6
+                (lesson, akademic_year) VALUES (lessonId, akademic_year_id);
 
-	INSERT INTO LessonCreateReportFormEntry7
-	(lesson) VALUES (lessonId);
+                INSERT INTO LessonCreateReportFormEntry7
+                (lesson, akademic_year) VALUES (lessonId, akademic_year_id);
 
-	INSERT INTO LessonCreateReportFormEntry8
-	(lesson) VALUES (lessonId);
+                INSERT INTO LessonCreateReportFormEntry8
+                (lesson, akademic_year) VALUES (lessonId, akademic_year_id);
 
-	INSERT INTO LessonCreateReportFormEntry9
-	(lesson) VALUES (lessonId);
+                INSERT INTO LessonCreateReportFormEntry9
+                (lesson, akademic_year) VALUES (lessonId, akademic_year_id);
 
-	INSERT INTO LessonCreateReportFormEntry11
-	(lesson) VALUES (lessonId);
+                INSERT INTO LessonCreateReportFormEntry11
+                (lesson, akademic_year) VALUES (lessonId, akademic_year_id);
 
-	INSERT INTO LessonCreateReportFormEntry12
-	(lesson) VALUES (lessonId);
+		INSERT INTO LessonCreateReportFormEntry11
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
 
-	INSERT INTO LessonCreateReportFormEntry13
-	(lesson) VALUES (lessonId);
+		INSERT INTO LessonCreateReportFormEntry12
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
 
-	INSERT INTO LessonCreateReportFormEntry14
-	(lesson) VALUES (lessonId);
+		INSERT INTO LessonCreateReportFormEntry13
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
+
+		INSERT INTO LessonCreateReportFormEntry14
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
 
 
-	INSERT INTO LessonCreateReportFormEntry15
-	(lesson) VALUES (lessonId);
+		INSERT INTO LessonCreateReportFormEntry15
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
 
-	INSERT INTO LessonCreateReportFormEntry16
-	(lesson) VALUES (lessonId);
+		INSERT INTO LessonCreateReportFormEntry16
+		(lesson, akademic_year) VALUES (lessonId, akademic_year_id);
+	END LOOP;
 END;
 $$ LANGUAGE plpgsql;
 `
